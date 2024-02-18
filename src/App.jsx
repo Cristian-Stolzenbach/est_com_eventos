4d995ffdd78e4bdd692eaf361bc7e7f4de8bd9a4
AGUSTIN <cristian-stolzenbach@hotmail.com>
2024-02-18 14:18:25 -0300

    SUBIENDO TRABAJO CON PROBLEMAS DE ESTILOS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Registro from './assets/components/Registro'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Registro />
+  )
+}
+
+export default App
